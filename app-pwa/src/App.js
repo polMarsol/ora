8cd99e527741fc463cd6194ec4e9941e624edcf3
PolMarsolUdl <144933485+polMarsol@users.noreply.github.com>
2025-06-03 22:00:46 +0200

    Update App.js

--- app-pwa/src/App.js
+++ app-pwa/src/App.js
@@ -19,7 +19,7 @@ const localizer = momentLocalizer(moment);
 
 // Defineix la URL base del backend
 // IMPORTANT: SUBSTITUEIX AQUESTA URL AMB LA TEVA URL DE BACKEND DE RENDER.COM O SIMILAR
-const API_BASE_URL = 'https://ora-rkes.onrender.com';//***************************************************************************************************************************************** */
+const API_BASE_URL = 'https://ora-44gf.onrender.com';//***************************************************************************************************************************************** */
 
 function App() {
   const [schedule, setSchedule] = useState([]);
